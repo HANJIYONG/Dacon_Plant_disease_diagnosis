430dd038274b9188a5a8de65582b0dc268f35376
HANJIYONG <jyong0719@gmail.com>
2022-02-06 21:30:35 +0900

    update files

---
+++ README.md
@@ -0,0 +1,65 @@
+# 농업 환경 변화에 따른 작물 병해 진단 AI 경진대회
+
+### Private score 7위 0.953044
+- 주최 : LG AI Research
+- 주관 : 데이콘
+- 주제 : "작물 환경 데이터"와 "작물 병해 이미지"를 이용해 "작물의 종류", "병해의 종류", "병해의 진행 정도"를 진단하는 AI 모델 개발
+- https://dacon.io/competitions/official/235870/overview/description
+
+## Development environment. 
+os : Windows 10 Pro
+
+##  Library version
+- numpy==1.19.5
+- opencv-python==4.5.4-dev
+- tqdm==4.43.0
+- scikit-learn==0.24.0
+- torch==1.10.0.dev20210803+cu111
+- torchvision==0.11.0.dev20210803+cu111
+- albumentations==0.5.2
+- timm==0.4.12
+
+## Model architecture
+<img src =https://user-images.githubusercontent.com/44361199/152677855-dd5b40a8-0175-4804-bce2-e6c699c61186.png    style="width:50%">
+
+## Key augmentation
+<img src =https://user-images.githubusercontent.com/44361199/152676731-c8a645e1-3ca5-4284-b7d1-e90b9d5d74c2.png    style="width:30%">
+
+## Directory tree
+
+```
+root
+│───Dacon_LG.ipynb
+│───utils.py
+└───data
+│   │───train
+│   │   |───10027
+│   │   │   |───10027.csv
+│   │   │   |───10027.jpg
+│   │   │   └───10027.json
+│   │   |───10027_focus
+│   │   │   |───10027_focus.csv
+│   │   │   |───10027_focus.jpg
+│   │   │   └───10027_focus.json
+│   │   │   ...
+│   │
+│   └───test
+│   │   |───10000
+│   │   │   |───10000.csv
+│   │   │   └───10000.jpg
+│   │   |───10001
+│   │   │   |───10001.csv
+│   │   │   └───10001.jpg
+│       │   ...
+│   
+└───sample_submission.csv
+```
+
+
+## Training
+
+- Dacon_LG.ipynb 파일이 존재하는 경로에 "data" 폴더와 utils.py파일이 존재 해야한다.
+- Dacon_LG.ipynb 파일의 Model Training까지 수행
+
+## Evaluation
+- Dacon_LG.ipynb 파일에서 Model Training을 제외한 나머지 코드 수행
---
+++ utils.py
@@ -0,0 +1,107 @@
+import math
+from torch.optim.lr_scheduler import _LRScheduler
+import numpy as np
+import torch
+
+class CosineAnnealingWarmUpRestarts(_LRScheduler):
+    def __init__(self, optimizer, T_0, T_mult=1, eta_max=0.1, T_up=0, gamma=1., last_epoch=-1):
+        if T_0 <= 0 or not isinstance(T_0, int):
+            raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
+        if T_mult < 1 or not isinstance(T_mult, int):
+            raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
+        if T_up < 0 or not isinstance(T_up, int):
+            raise ValueError("Expected positive integer T_up, but got {}".format(T_up))
+        self.T_0 = T_0
+        self.T_mult = T_mult
+        self.base_eta_max = eta_max
+        self.eta_max = eta_max
+        self.T_up = T_up
+        self.T_i = T_0
+        self.gamma = gamma
+        self.cycle = 0
+        self.T_cur = last_epoch
+        super(CosineAnnealingWarmUpRestarts, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if self.T_cur == -1:
+            return self.base_lrs
+        elif self.T_cur < self.T_up:
+            return [(self.eta_max - base_lr)*self.T_cur / self.T_up + base_lr for base_lr in self.base_lrs]
+        else:
+            return [base_lr + (self.eta_max - base_lr) * (1 + math.cos(math.pi * (self.T_cur-self.T_up) / (self.T_i - self.T_up))) / 2
+                    for base_lr in self.base_lrs]
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.cycle += 1
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = (self.T_i - self.T_up) * self.T_mult + self.T_up
+        else:
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                    self.cycle = epoch // self.T_0
+                else:
+                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                    self.cycle = n
+                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+                
+        self.eta_max = self.base_eta_max * (self.gamma**self.cycle)
+        self.last_epoch = math.floor(epoch)
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+            
+            
+class EarlyStopping:
+    """주어진 patience 이후로 validation loss가 개선되지 않으면 학습을 조기 중지"""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): validation loss가 개선된 후 기다리는 기간
+                            Default: 7
+            verbose (bool): True일 경우 각 validation loss의 개선 사항 메세지 출력
+                            Default: False
+            delta (float): 개선되었다고 인정되는 monitered quantity의 최소 변화
+                            Default: 0
+            path (str): checkpoint저장 경로
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''validation loss가 감소하면 모델을 저장한다.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
